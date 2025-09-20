184b41e645f1f2a805888160824b9eb8007ad9f3
officialbillxpert-svg <official.billxpert@gmail.com>
2025-09-20 20:59:30 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ BillXpert Converter en ligne !"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
